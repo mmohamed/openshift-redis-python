390b11503ba688c4f22cb74f20a5c2238d755081
MOHAMED Marouan <medmarouen@gmail.com>
2021-02-22 13:47:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def get(key):
   
 @api.route('/<key>', methods=['POST'])
 def set(key):  
-  return json.dumps({"status": r.set(request.data), "value": key})
+  return json.dumps({"status": r.set(key, request.data), "value": key})
     
 if __name__ == '__main__':
   api.run(host='0.0.0.0', port=8080)
