31cf0e180002baa6ad33feb317c6576ca3358510
MOHAMED Marouan <medmarouen@gmail.com>
2021-02-22 12:28:12 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import redis
+import os
+from flask import Flask, request
+
+r = redis.Redis(host='redis', port=6379, password=os.environ["REDIS_PASSWORD"])
+
+api = Flask(__name__)
+
+@api.route('/<key>', methods=['GET'])
+def get(key):  
+  return json.dumps({"status": True, "value": r.get(key)})
+  
+@api.route('/<key>', methods=['POST'])
+def set(key):  
+  return json.dumps({"status": r.set(request.data), "value": key})
+    
+if __name__ == '__main__':
+  api.run(host='0.0.0.0')
